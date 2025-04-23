44648e91fa49fb0933d4b9824c40c91be373a9d3
miranda <miranda.wqa@gmail.com>
2025-04-23 18:59:18 -0400

    ss

--- github-test-summary.js
+++ github-test-summary.js
@@ -38,7 +38,7 @@ const seconds = (stats.duration / 1000).toFixed(2);
 const resultSymbol = stats.failed > 0 ? '🔴' : '🟢';
 
 const html = `
-<h2>Playwright Results</h2>
+<h2>Playwright Test Run Summary</h2>
 <table>
   <tr>
     <th>Results</th>
