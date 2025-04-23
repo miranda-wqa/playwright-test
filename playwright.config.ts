d1cb42a9410bf3785a8c20c755e82952b9016182
miranda <miranda.wqa@gmail.com>
2025-04-23 10:59:28 -0400

    corrected reporter path

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { outputFolder: path.resolve('test-results/') }], 
-    ['json', { outputFile: path.resolve('test-results/json-report.json')}],
+    ['html', { outputFolder: path.resolve('test-reports/') }], 
+    ['json', { outputFile: path.resolve('test-reports/json-report.json')}],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
